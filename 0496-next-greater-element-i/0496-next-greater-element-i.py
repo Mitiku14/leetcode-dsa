f8d3aeeed38d76030106c0126d9091a4520abd27
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2025-11-08 12:45:09 +0300

    Time: 21 ms (18.48%), Space: 18.1 MB (20.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,24 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        numsIndex = {n: i for i, n in enumerate(nums1)}
-        res = [-1] * len(nums1)
-        for i in range(len(nums2)):
-            if nums2[i] not in numsIndex:
-                continue
-            for j in range(i +1, len(nums2)):
-                if nums2[j] > nums2[i]:
-                    idx = numsIndex[nums2[i]]
-                    res[idx] = nums2[j]
+        ans = []
+        for num in nums1:
+            index = nums2.index(num)
+            next_greater = -1
+            for j in range(index +1, len(nums2)):
+                if nums2[j] > num:
+                    next_greater = nums2[j]
                     break
-        return res
-                
-       
+        
+            ans.append(next_greater)
+        
+        return ans
+
+        
+        
+
+
+
+
+
+            
+        
