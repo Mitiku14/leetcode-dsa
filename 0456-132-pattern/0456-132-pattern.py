a8e1804dc55961f8bb5be61c04700e50b32bf070
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2025-11-11 22:52:47 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,11 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        if nums == sorted(nums):
+            return False
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] < nums[k] and nums[k] < nums[j]:
+                        return True
+        return False
+        
