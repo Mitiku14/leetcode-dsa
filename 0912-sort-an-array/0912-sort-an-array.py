53e1ee1f49b7e3eee0caf4fd7f74b89214c63bf5
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2026-04-07 18:59:23 +0300

    Time: 616 ms (60.79%), Space: 28.5 MB (31.03%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,17 +1,27 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        # we use shell sort for this quesion 
+        def merge(left_half, right_half):
+            i = 0
+            j = 0
+            res = []
+            while i < len(left_half) and j < len(right_half):
+                if left_half[i] < right_half[j]:
+                    res.append(left_half[i])
+                    i += 1
+                else:
+                    res.append(right_half[j])
+                    j += 1
+            res.extend(left_half[i:])
+            res.extend(right_half[j:])
+            return res
         
-        gap = n//2
-        while gap > 0:
-            for i in range(gap, n):
-                temp = nums[i]
-                j = i
-                while j >= gap and nums[j-gap] > temp:
-                    nums[j] = nums[j-gap]
-                    j -= gap
-                nums[j] = temp
-            gap //= 2
-        return nums
-                
+        def mergeSort(left, right, arr):
+            if left == right:
+                return [arr[left]]
+            mid = left + (right - left) // 2
+            left_half = mergeSort(left, mid, arr)
+            right_half = mergeSort(mid + 1, right, arr)
+            return merge(left_half, right_half)
+        return mergeSort(0, n -1, nums)
+        
