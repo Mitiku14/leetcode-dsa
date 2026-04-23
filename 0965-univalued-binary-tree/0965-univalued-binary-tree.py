6bc4a08d8d0fae3b8931f0941117660aea892687
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2026-04-23 17:17:37 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.py
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.py
@@ -7,17 +7,22 @@
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
         q = deque([root])
-    
         res = []
+        prev = None
         while q:
             size = len(q)
             for i in range(size):
                 node = q.popleft()
+                if prev != None and node.val != prev:
+                    return False
+                prev = node.val
                 res.append(node.val)
+                
+
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
-                    
+
         
         return all(res[0] == res[i] for i in range(1, len(res)))
