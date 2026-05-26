09eda049301eb599d58a27d2b8f8f3f074949bea
Mitiku tadesse <149384297+Mitiku14@users.noreply.github.com>
2026-05-26 21:22:40 +0300

    Time: 0 ms (100%), Space: 19.4 MB (84.79%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,27 +1,32 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        n = len(image)
-        m = len(image[0])
+    def floodFill(self, image, sr, sc, color):
+        rows = len(image)
+        cols = len(image[0])
         start = image[sr][sc]
-        dirs = [(-1, 0),  (1, 0), (0, -1), (0, 1)]
-        visited = [[False] * m for _ in range(n)]
-        image[sr][sc] = color
-        def inbound(row, col):
-            return 0 <= row < n and 0 <= col < m
-        def dfs(r, c):
-            visited[r][c] = True
-            
-            for dr, dc in dirs:
-                nr, nc = r + dr, c + dc
-                if not inbound(nr, nc) or visited[nr][nc]:
-                    continue
-                if image[nr][nc] == start:
-                    image[nr][nc] = color
-                    dfs(nr, nc)
-                visited[nr][nc] = True
-        
-        dfs(sr, sc)
-        return image
-                
+        if start == color:
+            return image
 
+        dirs = [
+            (-1, 0),  
+            (1, 0),   
+            (0, -1),  
+            (0, 1)    
+        ]
+
+        def inbound(r, c):
+            return 0 <= r < rows and 0 <= c < cols
+        q = deque()
+        q.append((sr, sc))
+        image[sr][sc] = color
+        while q:
+            r, c = q.popleft()
+            for dr, dc in dirs:
+                nr , nc = r + dr, c + dc
+                if inbound(nr, nc ) and image[nr][nc] == start:
+                    image[nr][nc] = color
+                    q.append((nr, nc))
         
+        return image
+
+                
+            
